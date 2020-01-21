d8222763a5c2d26fbdc4c73723c8f93a4381fa0a
h7seiji <helder.costa@usp.br>
2020-01-21 18:59:01 -0300

    fix uuid

--- addons/analytics/config.py
+++ addons/analytics/config.py
@@ -1,11 +1,11 @@
 import bpy
 import datetime
 import getpass
-import uuid
+
 
 version = '1.0'
 api_url = "https://api.real2u.com.br/blender/analytics"
+# api_url = "http://localhost:4000/blender/analytics"
 # blend_file = bpy.path.basename(bpy.context.blend_data.filepath)
-event_id = str(uuid.uuid1())
 timestamp = datetime.datetime.now().isoformat()
 user = getpass.getuser()
