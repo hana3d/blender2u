11ead359368c28a3bf655542fa34853422175526
h7seiji <helder.costa@usp.br>
2020-01-21 15:18:58 -0300

    Analytics 1.0

---
+++ addons/analytics/__init__.py
@@ -0,0 +1,51 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name": "analytics",
+    "author": "real2u",
+    "description": "",
+    "blender": (2, 80, 0),
+    "version": (1, 0, 0),
+    "location": "",
+    "warning": "",
+    "category": "System"
+}
+
+import bpy
+import os
+import datetime
+import atexit
+from .blend import blend_handler, BlendModal
+# from .events import event_handler, EventModal
+# from .reports import ReportModal
+
+
+def register():
+    bpy.utils.register_class(BlendModal)
+    # bpy.utils.register_class(EventModal)
+    # bpy.utils.register_class(ReportModal)
+    bpy.app.handlers.load_post.append(blend_handler)
+    # bpy.app.handlers.load_post.append(event_handler)
+
+
+def unregister():
+    # bpy.app.handlers.load_post.remove(event_handler)
+    bpy.app.handlers.load_post.remove(blend_handler)
+    # bpy.utils.unregister_class(ReportModal)
+    # bpy.utils.unregister_class(EventModal)
+    bpy.utils.unregister_class(BlendModal)
+
+
+if __name__ == "__main__":
+    register()
---
+++ addons/analytics/blend.py
@@ -0,0 +1,68 @@
+import bpy
+import os
+import json
+import requests
+from bpy.app.handlers import persistent
+from .config import version, api_url, event_id, timestamp, user
+
+
+@persistent
+def blend_handler(dummy):
+    if bpy.path.basename(bpy.context.blend_data.filepath) != '':
+        bpy.ops.object.blend_modal('INVOKE_DEFAULT')
+
+
+class BlendModal(bpy.types.Operator):
+    bl_idname = "object.blend_modal"
+    bl_label = "Blend Modal Operator"
+
+    def __init__(self):
+        print("Start")
+
+        blend_file = bpy.path.basename(bpy.context.blend_data.filepath)
+        blender_version = bpy.app.version_string
+
+        data = {
+            'blend': blend_file,
+            'blender_version': blender_version,
+            'event_id': event_id,
+            'timestamp': timestamp,
+            'user': user,
+            'version': version,
+            'open': 1
+        }
+
+        r = requests.post(url=api_url, json=data)
+        print(r.status_code)
+
+    def __del__(self):
+        print("End")
+
+        blend_file = bpy.path.basename(bpy.context.blend_data.filepath)
+        blender_version = bpy.app.version_string
+
+        data = {
+            'blend': blend_file,
+            'blender_version': blender_version,
+            'event_id': event_id,
+            'timestamp': timestamp,
+            'user': user,
+            'version': version,
+            'close': 1
+        }
+
+        r = requests.post(url=api_url, json=data)
+        print(r.status_code)
+
+    def execute(self, context):
+
+        return {'FINISHED'}
+
+    def modal(self, context, event):
+
+        return {'PASS_THROUGH'}
+
+    def invoke(self, context, event):
+        context.window_manager.modal_handler_add(self)
+
+        return {'RUNNING_MODAL'}
