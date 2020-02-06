31e03eb78cd9b677494de58b03d2757d9e2f13e9
h7seiji <helder.costa@usp.br>
2020-02-06 15:45:15 -0300

    format code

--- addons/computer_vision/opencv.py
+++ addons/computer_vision/opencv.py
@@ -8,24 +8,27 @@ class OpencvClass(bpy.types.Operator):
     bl_label = "OpenCV Class"
     bl_options = {'REGISTER', 'UNDO'}
 
+    def find_contours(image_path):
+        img = cv2.imread(image_path, 1)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+        morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
+
+        cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        cnt = sorted(cnts, key=cv2.contourArea)[-1]
+
+        return cnt
+
     def execute(self, context):
         obj = bpy.context.active_object
 
         if hasattr(obj.data, 'filepath'):
             image_path = obj.data.filepath
 
-            img = cv2.imread(image_path, 1)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
-
-            # (2) Morph-op to remove noise
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-            morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
-
-            # (3) Find the max-area contour
-            cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
-            cnt = sorted(cnts, key=cv2.contourArea)[-1]
+            cnt = self.find_contours(image_path)
 
             print(cnt)
 
