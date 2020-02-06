a54db6df7afd53df5eb1969be2af7092c1a5fb92
h7seiji <helder.costa@usp.br>
2020-02-06 15:35:53 -0300

    find contour

--- addons/computer_vision/opencv.py
+++ addons/computer_vision/opencv.py
@@ -14,10 +14,19 @@ class OpencvClass(bpy.types.Operator):
         if hasattr(obj.data, 'filepath'):
             image_path = obj.data.filepath
 
-            image = cv2.imread(image_path, 1)
+            img = cv2.imread(image_path, 1)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
 
-            back_sub = cv2.createBackgroundSubtractorMOG2()
+            # (2) Morph-op to remove noise
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+            morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
 
-            fg_mask = back_sub.apply(image)
+            # (3) Find the max-area contour
+            cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
+            cnt = sorted(cnts, key=cv2.contourArea)[-1]
+
+            print(cnt)
 
         return {'FINISHED'}
