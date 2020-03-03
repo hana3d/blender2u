3e8428795a365d015edaabf313824b245bbdbf66
h7seiji <helder.costa@usp.br>
2020-03-03 15:18:15 -0300

    better version.sh

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 input="./__init__.py"
 version=`sed "6q;d" $input`
-# echo $((${version:22:1}+1))
-sed "6 s/.*/    \"version\": (1, 4, $((${version:22:1}+1))),/" $input > ./temp.py
+version=$(echo "${version:22:3}" | sed 's/[), ]//g')
+sed "6 s/.*/    \"version\": (1, 4, $(($version+1))),/" $input > ./temp.py
 mv ./temp.py ./__init__.py
