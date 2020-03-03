41fad3e0139b7d12211b75f1f4a6a9d488cf9e9c
h7seiji <helder.costa@usp.br>
2020-03-03 15:29:57 -0300

    update version.sh

--- version.sh
+++ version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 input="./__init__.py"
-version=`sed "6q;d" $input`
-version=$(echo "${version:22:3}" | sed 's/[), ]//g')
-sed "6 s/.*/    \"version\": (1, 4, $(($version+1))),/" $input > ./temp.py
+line=`sed "6q;d" $input`
+version0=$(echo "${line:15:3}" | sed 's/[(, ]//g')
+version1=$(echo "${line:19:3}" | sed 's/[, ]//g')
+version2=$(echo "${line:22:3}" | sed 's/[), ]//g')
+sed "6 s/.*/    \"version\": ($version0, $version1, $(($version2+1))),/" $input > ./temp.py
 mv ./temp.py ./__init__.py
