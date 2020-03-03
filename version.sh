e32934683ae27d2ba4c28d9a5247f956214322b2
h7seiji <helder.costa@usp.br>
2020-03-03 13:46:53 -0300

    update version shell script

--- version.sh
+++ version.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 input="./__init__.py"
-sed '6q;d' $input
+version=`sed "6q;d" $input`
+# echo $((${version:22:1}+1))
+sed "6 s/.*/    \"version\": (1, 4, $((${version:22:1}+1))),/" $input > ./temp.py
+mv ./temp.py ./__init__.py
