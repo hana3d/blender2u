50c21d41284ef7144416999d0ce4704b53405975
h7seiji <helder.costa@usp.br>
2020-03-03 16:08:11 -0300

    tag in ci

---
+++ tag.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+input="./__init__.py"
+line=`sed "6q;d" $input`
+
+tmp="${line%%(*}"
+if [ "$tmp" != "$line" ]; then
+  line=$(echo "${line:$((${#tmp}+1))}")
+fi
+tmp="${line%%,*}"
+if [ "$tmp" != "$line" ]; then
+  version0=$(echo "${line:0:$((${#tmp}))}")
+  line=$(echo "${line:$((${#tmp}+2))}")
+fi
+tmp="${line%%,*}"
+if [ "$tmp" != "$line" ]; then
+  version1=$(echo "${line:0:$((${#tmp}))}")
+  line=$(echo "${line:$((${#tmp}+2))}")
+fi
+tmp="${line%%)*}"
+if [ "$tmp" != "$line" ]; then
+  version2=$(echo "${line:0:$((${#tmp}))}")
+fi
+
+version="$version0.$version1.$version2"
+
+curl -X POST --silent --show-error --fail \
+  "https://gitlab.my.org/api/v4/projects/16221229/repository/tags?tag_name=$version&private_token=${GITLAB_TOKEN}"
