e5b2004756f413fbd4986921f60c8b4cc7bed848
h7seiji <helder.costa@usp.br>
2020-03-05 16:08:29 -0300

    update version

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author": "Real2U",
     "description": "Add-ons developed by Real2U",
     "blender": (2, 80, 0),
-    "version": (1, 4, 126),
+    "version": (1, 4, 127),
     "location": "",
     "warning": "",
     "wiki_url": "https://gitlab.com/real2u/blender2u",
