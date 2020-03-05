56a5e2145f01f1b83d8f2ee8229f390f1ad1b122
h7seiji <helder.costa@usp.br>
2020-03-05 14:18:40 -0300

    raise error

--- tests/install.py
+++ tests/install.py
@@ -7,7 +7,7 @@ class NewError(Exception):
 
 
 def handle_error():
-    NewError
+    raise NewError
 
 
 addon_utils.enable("blender2u", default_set=False, persistent=True, handle_error=handle_error())
