228887c243a08aa1da5c60dea78185aa3e1b3491
h7seiji <helder.costa@usp.br>
2020-03-05 14:09:10 -0300

    error class

--- tests/install.py
+++ tests/install.py
@@ -1,3 +1,13 @@
 import addon_utils
 
-addon_utils.enable("blender2u", default_set=False, persistent=True, handle_error=None)
+
+class NewError(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+def handle_error():
+    NewError
+
+
+addon_utils.enable("blender2u", default_set=False, persistent=True, handle_error=handle_error())
