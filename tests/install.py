18fe82780a6c72647195b8639463d1c6f6639fee
h7seiji <helder.costa@usp.br>
2020-03-05 14:26:00 -0300

    try different error handling

--- tests/install.py
+++ tests/install.py
@@ -1,10 +1,15 @@
 import addon_utils
 
 
-class NewError(Exception):
+class Error(Exception):
     """Base class for other exceptions"""
     pass
 
 
+class InstallError(Error):
+    """Raised when the install fails"""
+    pass
+
+
 if addon_utils.enable("blender2u", default_set=False, persistent=True, handle_error=None) is None:
-    raise NewError
+    raise InstallError
