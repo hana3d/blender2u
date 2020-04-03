9f160caf02d38e8cd54ea980b18fe3b8b1d8cf81
h7seiji <helder_seiji@hotmail.com>
2020-04-03 12:41:00 -0300

    better path finding

--- addons/material_library/api.py
+++ addons/material_library/api.py
@@ -8,6 +8,8 @@ from .materials_library_vx import refresh_libs
 api_url = "http://localhost:4000"
 # api_url = "https://staging-api.real2u.com.br/blender"
 
+matlib_path = os.path.dirname(__file__)
+
 
 class S3Download(bpy.types.Operator):
     """S3 Download"""
@@ -16,26 +18,6 @@ class S3Download(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        if os.path.exists(bpy.utils.resource_path('USER').replace(' ', '') + os.sep + 'scripts' + os.sep + 'addons'
-                          + os.sep + 'blender2u' + os.sep + 'addons' + os.sep + 'material_library'):
-            matlib_path = bpy.utils.resource_path('USER').replace(' ', '') + os.sep + 'scripts' + os.sep + 'addons' \
-                + os.sep + 'blender2u' + os.sep + 'addons' + os.sep + 'material_library'
-        elif os.path.exists(bpy.utils.resource_path('USER') + os.sep + 'scripts' + os.sep + 'addons'
-                            + os.sep + 'blender2u' + os.sep + 'addons' + os.sep + 'material_library'):
-            matlib_path = bpy.utils.resource_path('USER') + os.sep + 'scripts' + os.sep + 'addons' \
-                + os.sep + 'blender2u' + os.sep + 'addons' + os.sep + 'material_library'
-        elif os.path.exists(bpy.utils.resource_path('USER').replace(' ', '') + os.sep + 'scripts' + os.sep + 'addons'
-                            + os.sep + 'material_library'):
-            matlib_path = bpy.utils.resource_path('USER').replace(' ', '') + os.sep + 'scripts' + os.sep + 'addons' \
-                + os.sep + 'material_library'
-        elif os.path.exists(bpy.utils.resource_path('USER') + os.sep + 'scripts' + os.sep + 'addons'
-                            + os.sep + 'material_library'):
-            matlib_path = bpy.utils.resource_path('USER') + os.sep + 'scripts' + os.sep + 'addons' \
-                + os.sep + 'material_library'
-        else:
-            self.report({'ERROR'}, "matlib path not found")
-            return {'CANCELLED'}
-
         signed_url_request = requests.get(url=(api_url + '/matlib-get'))
         with urllib.request.urlopen(signed_url_request.json()) as response, open(matlib_path + os.sep + 'cycles_materials.blend', 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
