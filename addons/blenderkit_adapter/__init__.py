90f2bb5989001cd7c4b5b52c00afe6be7c83a49a
Felipe Chamas Biondi <felipe@real2u.com.br>
2020-06-08 18:08:07 +0000

    Upload e Search de cena

--- addons/blenderkit_adapter/__init__.py
+++ addons/blenderkit_adapter/__init__.py
@@ -7,17 +7,17 @@ bl_info = {
 }
 
 import os
-import copy
 
 import bpy
 import blenderkit
 
-from . import ui, custom_props, download, search
+from . import annotations, ui, ui_scene, custom_props, download, search
 
 
 get_bkit_url = blenderkit.paths.get_bkit_url
 get_upload_data = blenderkit.upload.get_upload_data
 model_search_config = blenderkit.BlenderKitModelSearchProps.__annotations__
+material_search_config = blenderkit.BlenderKitMaterialSearchProps.__annotations__
 model_upload_props = blenderkit.BlenderKitModelUploadProps.__annotations__
 material_upload_props = blenderkit.BlenderKitMaterialUploadProps.__annotations__
 upload_operator = blenderkit.upload.UploadOperator.__annotations__
@@ -42,41 +42,16 @@ def get_upload_data2(self, context, asset_type):
     export_data, upload_data, eval_path_computing, eval_path_state, eval_path, props = get_upload_data(self, context,
                                                                                                        asset_type)
 
-    upload_data['client'] = props.client
-    upload_data['sku'] = props.sku
-
+    if hasattr(props, 'client'):
+        upload_data['client'] = props.client
+    if hasattr(props, 'sku'):
+        upload_data['sku'] = props.sku
     if hasattr(props, 'custom_props'):
         upload_data['metadata'] = {}
         for key in props.custom_props.keys():
             upload_data['metadata'][key] = props.custom_props[key]
 
     return export_data, upload_data, eval_path_computing, eval_path_state, eval_path, props
-
-
-def change_annotations(class_, original_annotations, change_function, assigned_property=None):
-    if getattr(class_, "is_registered"):
-        annotations = copy.deepcopy(original_annotations)
-
-        change_function(annotations)
-
-        class_.__annotations__ = annotations
-
-        bpy.utils.unregister_class(class_)
-        bpy.utils.register_class(class_)
-
-        if assigned_property is not None:
-            exec(f'{assigned_property}=bpy.props.PointerProperty(type={class_.__module__}.{class_.__name__})')
-
-
-def restore_annotations(class_, original_annotations, assigned_property=None):
-    if getattr(class_, "is_registered"):
-        class_.__annotations__ = original_annotations
-
-        bpy.utils.unregister_class(class_)
-        bpy.utils.register_class(class_)
-
-        if assigned_property is not None:
-            exec(f'{assigned_property}=bpy.props.PointerProperty(type={class_.__module__}.{class_.__name__})')
 
 
 def change_default_append(annotations):
@@ -90,40 +65,67 @@ def create_custom_props(annotations):
     annotations['custom_props'] = bpy.props.PointerProperty(type=custom_props.CustomPropsPropertyGroup)
 
 
+def change_default_automap(annotations):
+    annotations['automap'][1]['default'] = False
+
+
 def register():
     custom_props.register()
     blenderkit.paths.get_bkit_url = get_bkit_url2
     blenderkit.upload.get_upload_data = get_upload_data2
-    change_annotations(
+    annotations.change_annotations(
         blenderkit.BlenderKitModelSearchProps,
         model_search_config,
         change_default_append,
         'bpy.types.Scene.blenderkit_models'
     )
-    change_annotations(
+    annotations.change_annotations(
         blenderkit.BlenderKitModelUploadProps,
         model_upload_props,
         create_custom_props,
         'bpy.types.Object.blenderkit'
     )
-    change_annotations(
+    annotations.change_annotations(
         blenderkit.BlenderKitMaterialUploadProps,
         material_upload_props,
         create_custom_props,
         'bpy.types.Material.blenderkit'
     )
+    annotations.change_annotations(
+        blenderkit.BlenderKitMaterialSearchProps,
+        material_search_config,
+        change_default_automap,
+        'bpy.types.Scene.blenderkit_mat'
+    )
+
     search.register()
     download.register()
     ui.register()
+    ui_scene.register()
 
 
 def unregister():
+    ui_scene.unregister()
     ui.unregister()
+    annotations.restore_annotations(
+        blenderkit.BlenderKitMaterialSearchProps,
+        material_search_config,
+        'bpy.types.Scene.blenderkit_mat'
+    )
+    annotations.restore_annotations(
+        blenderkit.BlenderKitMaterialUploadProps,
+        material_upload_props,
+        'bpy.types.Material.blenderkit')
+    annotations.restore_annotations(
+        blenderkit.BlenderKitModelUploadProps,
+        model_upload_props,
+        'bpy.types.Object.blenderkit')
+    annotations.restore_annotations(
+        blenderkit.BlenderKitModelSearchProps,
+        model_search_config,
+        'bpy.types.Scene.blenderkit_models')
     download.unregister()
     search.unregister()
-    restore_annotations(blenderkit.BlenderKitMaterialUploadProps, material_upload_props, 'bpy.types.Material.blenderkit')
-    restore_annotations(blenderkit.BlenderKitModelUploadProps, model_upload_props, 'bpy.types.Object.blenderkit')
-    restore_annotations(blenderkit.BlenderKitModelSearchProps, model_search_config, 'bpy.types.Scene.blenderkit_models')
     blenderkit.upload.get_upload_data = get_upload_data
     blenderkit.paths.get_bkit_url = get_bkit_url
     custom_props.unregister()
