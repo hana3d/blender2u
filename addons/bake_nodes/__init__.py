a621242f9ca8e1270d334909474aeac56dd82dae
h7seiji <helder_seiji@hotmail.com>
2021-05-04 13:49:16 -0300

    more feedback changes

--- addons/bake_nodes/__init__.py
+++ addons/bake_nodes/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author": "R2U",
     "description": "",
     "blender": (2, 80, 0),
-    "version": (0, 4, 2),
+    "version": (0, 4, 3),
     "location": "",
     "warning": "",
     "category": "Material"
@@ -43,7 +43,7 @@ class BakeNodesProps(bpy.types.PropertyGroup):
     )
 
     bake_metallic: bpy.props.BoolProperty(
-        name='Mettalic',
+        name='Metallic',
         description='Bake nodes connected to Mettalic input',
         default=False,
     )
@@ -60,60 +60,11 @@ class BakeNodesProps(bpy.types.PropertyGroup):
         default=False,
     )
 
-
-def bake_nodes(mat: bpy.types.Material, bake_type: str, input_socket: int, color_space: str):
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.ops.mesh.primitive_plane_add(location=(789, 789, 789))
-    plane = bpy.context.active_object
-    plane.active_material = mat
-
-    plane.select_set(True)
-    node_tree = plane.active_material.node_tree
-
-    emission_node = node_tree.nodes.new('ShaderNodeEmission')
-    emission_node.inputs[1].default_value = 1
-
-    if bake_type == 'NORMAL':
-        src_node = node_tree.nodes['Normal Map']
-    else:
-        src_node = node_tree.nodes['Principled BSDF']
-
-    for link in node_tree.links:
-        if (
-            link.to_node == src_node
-            and link.to_socket == src_node.inputs[input_socket]
-        ):
-            output_socket = link.from_socket
-    node_tree.links.new(
-        emission_node.outputs[0],
-        node_tree.nodes['Material Output'].inputs[0]
+    resolution: bpy.props.IntProperty(
+        name='Resolution',
+        description='Resolution of the baked image',
+        default=1024,
     )
-    node_tree.links.new(
-        output_socket,
-        emission_node.inputs[0]
-    )
-
-    node = node_tree.nodes.new('ShaderNodeTexImage')
-    node.select = True
-    node_tree.nodes.active = node
-    new_img = bpy.data.images.new(f'{mat.name}_{bake_type}', 1024, 1024)
-    new_img.colorspace_settings.name = color_space
-    new_img.use_fake_user = True
-    node.image = new_img
-
-    bpy.ops.object.bake(type='EMIT', save_mode='EXTERNAL')
-
-    node_tree.links.new(node.outputs[0], src_node.inputs[input_socket])
-
-    node_tree.links.new(
-        node_tree.nodes['Principled BSDF'].outputs[0],
-        node_tree.nodes['Material Output'].inputs[0]
-    )
-
-    node_tree.nodes.remove(emission_node)
-
-    bpy.ops.object.delete(use_global=True, confirm=False)
-    bpy.ops.object.select_all(action='DESELECT')
 
 
 class BakeNodes(bpy.types.Operator):
@@ -122,13 +73,86 @@ class BakeNodes(bpy.types.Operator):
     bl_label = 'Bake Nodes'
     bl_options = {'REGISTER', 'UNDO'}
 
+    def bake_nodes(
+        self,
+        mat: bpy.types.Material,
+        bake_type: str,
+        input_socket: int,
+        color_space: str,
+        resolution: int,
+    ):
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.mesh.primitive_plane_add(location=(789, 789, 789))
+        plane = bpy.context.active_object
+        plane.active_material = mat
+
+        plane.select_set(True)
+        node_tree = plane.active_material.node_tree
+
+        emission_node = node_tree.nodes.new('ShaderNodeEmission')
+        emission_node.inputs[1].default_value = 1
+
+        if bake_type == 'NORMAL':
+            src_node = node_tree.nodes['Normal Map']
+        else:
+            src_node = node_tree.nodes['Principled BSDF']
+
+        for link in node_tree.links:
+            if (
+                link.to_node == src_node
+                and link.to_socket == src_node.inputs[input_socket]
+            ):
+                output_socket = link.from_socket
+        node_tree.links.new(
+            emission_node.outputs[0],
+            node_tree.nodes['Material Output'].inputs[0]
+        )
+        try:
+            node_tree.links.new(
+                output_socket,
+                emission_node.inputs[0]
+            )
+        except UnboundLocalError:
+            self.report({'ERROR_INVALID_INPUT'}, f'No nodes in {bake_type} input')
+            return
+
+        node = node_tree.nodes.new('ShaderNodeTexImage')
+        node.select = True
+        node_tree.nodes.active = node
+        new_img = bpy.data.images.new(f'{mat.name}_{bake_type}', resolution, resolution)
+        new_img.colorspace_settings.name = color_space
+        new_img.use_fake_user = True
+        node.image = new_img
+
+        bpy.ops.object.bake(type='EMIT', save_mode='EXTERNAL')
+
+        node_tree.links.new(node.outputs[0], src_node.inputs[input_socket])
+
+        node_tree.links.new(
+            node_tree.nodes['Principled BSDF'].outputs[0],
+            node_tree.nodes['Material Output'].inputs[0]
+        )
+
+        node_tree.nodes.remove(emission_node)
+
+        bpy.ops.object.delete(use_global=True, confirm=False)
+        bpy.ops.object.select_all(action='DESELECT')
+
     def execute(self, context):
+        selected_objects = context.selected_objects
+        active_object = context.active_object
+
         context.scene.render.engine = 'CYCLES'
         context.scene.cycles.device = 'CPU'
         context.scene.cycles.bake_type = 'EMIT'
         context.scene.cycles.samples = 32
 
-        mat = bpy.data.materials[context.scene.bake_nodes.selected_material_index]
+        mat_index = context.scene.bake_nodes.selected_material_index
+        try:
+            mat = context.active_object.material_slots[mat_index].material
+        except IndexError:
+            self.report({'ERROR_INVALID_INPUT'}, f'Select a valid material to bake')
+            return {'CANCELLED'}
 
         with suppress(AttributeError):
             if context.scene.bake_nodes.bake_diffuse:
@@ -136,19 +160,25 @@ class BakeNodes(bpy.types.Operator):
                 context.scene.render.bake.use_pass_indirect = False
                 context.scene.render.bake.use_pass_color = True
                 context.scene.sequencer_colorspace_settings.name = 'sRGB'
-                bake_nodes(mat, 'DIFFUSE', 0, 'sRGB')
+                self.bake_nodes(mat, 'DIFFUSE', 0, 'sRGB', context.scene.bake_nodes.resolution)
 
             if context.scene.bake_nodes.bake_metallic:
                 context.scene.sequencer_colorspace_settings.name = 'Non-Color'
-                bake_nodes(mat, 'METALLIC', 4, 'Non-Color')
+                self.bake_nodes(mat, 'METALLIC', 4, 'Non-Color',
+                                context.scene.bake_nodes.resolution)
 
             if context.scene.bake_nodes.bake_roughness:
                 context.scene.sequencer_colorspace_settings.name = 'Non-Color'
-                bake_nodes(mat, 'ROUGHNESS', 7, 'Non-Color')
+                self.bake_nodes(mat, 'ROUGHNESS', 7, 'Non-Color',
+                                context.scene.bake_nodes.resolution)
 
             if context.scene.bake_nodes.bake_normal:
                 context.scene.sequencer_colorspace_settings.name = 'Non-Color'
-                bake_nodes(mat, 'NORMAL', 1, 'Non-Color')
+                self.bake_nodes(mat, 'NORMAL', 1, 'Non-Color', context.scene.bake_nodes.resolution)
+
+        for obj in selected_objects:
+            obj.select_set(True)
+        context.view_layer.objects.active = active_object
 
         return {'FINISHED'}
 
--- addons/bake_nodes/panel.py
+++ addons/bake_nodes/panel.py
@@ -1,8 +1,7 @@
 import bpy
-from bpy.types import Panel
 
 
-class OBJECT_PT_NodesPanel(Panel):
+class OBJECT_PT_NodesPanel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_label = 'Bake Nodes'
@@ -18,8 +17,8 @@ class OBJECT_PT_NodesPanel(Panel):
         layout.template_list(
             'UI_UL_list',
             'nodes_panel',
-            bpy.data,
-            'materials',
+            context.active_object,
+            'material_slots',
             context.scene.bake_nodes,
             'selected_material_index',
             rows=3,
@@ -28,4 +27,5 @@ class OBJECT_PT_NodesPanel(Panel):
         layout.prop(context.scene.bake_nodes, 'bake_metallic')
         layout.prop(context.scene.bake_nodes, 'bake_roughness')
         layout.prop(context.scene.bake_nodes, 'bake_normal')
+        layout.prop(context.scene.bake_nodes, 'resolution')
         layout.operator('object.bake_nodes', text='Bake Nodes', icon='MOD_BOOLEAN')
